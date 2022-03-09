e94f565e61e8d886c7ba409ba4dedb4ecf5656ed
RaudaJairo <18307060@utcgg.edu.mx>
2022-03-09 15:04:09 -0600

    Implement sequelize-bcrypt

--- src/database/models/user.model.js
+++ src/database/models/user.model.js
@@ -1,34 +1,37 @@
 const {DataTypes} = require("@sequelize/core");
+const useBcrypt = require('sequelize-bcrypt');
 const nanoid = require("../../lib/nanoid");
 
 const userModel = (sequelize) =>
-    sequelize.define("User", {
-        id: {
-            primaryKey: true,
-            type: DataTypes.STRING,
-            defaultValue: nanoid
-        },
-        email: {
-            type: DataTypes.STRING,
-            validate: {
-                isEmail: true
+    useBcrypt(sequelize.define("User",
+        {
+            id: {
+                primaryKey: true,
+                type: DataTypes.STRING,
+                defaultValue: nanoid
             },
-            unique: true
-        },
-        password: {
-            type: DataTypes.STRING
-        },
-        username: {
-            type: DataTypes.STRING,
-            unique: true,
-            validate: {
-                is: /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{3,29}$/i
+            email: {
+                type: DataTypes.STRING,
+                validate: {
+                    isEmail: true
+                },
+                unique: true
+            },
+            password: {
+                type: DataTypes.STRING
+            },
+            username: {
+                type: DataTypes.STRING,
+                unique: true,
+                validate: {
+                    is: /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{3,29}$/i
+                }
+            },
+            name: {
+                type: DataTypes.STRING,
+                validate: {isAlpha: true}
             }
-        },
-        name: {
-            type: DataTypes.STRING,
-            validate: {isAlpha: true}
         }
-    })
+    ));
 
 module.exports = userModel;
